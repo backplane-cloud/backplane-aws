147342ab18fc35a1c881a91b72841ecc26e148a2
Lewis Sheridan <lewis@backplane.cloud>
2024-03-20 21:47:59 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import asyncHandler from "express-async-handler";
+
+// AWS CODE
+const createAWSEnvironments = asyncHandler(async (req, res) => {
+  console.log("Create AWS Environments");
+  res.send("createAWSEnvironments Not yet implmented");
+  res.end;
+});
+
+const getAWSAccess = asyncHandler(async (req, res) => {
+  console.log("Get AWS Access");
+  res.send("getAWSAccess Not yet implmented");
+  res.end;
+});
+
+const getAWSCost = asyncHandler(async (req, res) => {
+  console.log("Get AWS Cost");
+  res.send("getAWSCost Not yet implmented");
+  res.end;
+});
+
+const getAWSPolicy = asyncHandler(async (req, res) => {
+  console.log("Get AWS Policy");
+  res.send("getAWSCost Not yet implmented");
+  res.end;
+});
+
+export { getAWSAccess, getAWSCost, getAWSPolicy, createAWSEnvironments };
